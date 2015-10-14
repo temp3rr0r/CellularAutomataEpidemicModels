d559028405245ab95913c35d0111b704c95a1f0a
TeMp3rr0r <madks@yahoo.gr>
2015-10-14 12:13:43 +0200

    Working SIS 2D GoL

--- GameOfLife2D.py
+++ GameOfLife2D.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import pylab as pl
 from operator import itemgetter
+from copy import deepcopy
 
 def getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours):
     newState = '0'
@@ -11,17 +12,18 @@ def getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours):
     rightCharacter = currentRowNeighbours[2]
 
     upperLeftCharacter = upperRowNeighbours[0]
-    upperCenterCharacter = upperRowNeighbours[0]
-    upperRightCharacter = upperRowNeighbours[0]
+    upperCenterCharacter = upperRowNeighbours[1]
+    upperRightCharacter = upperRowNeighbours[2]
 
     lowerLeftCharacter = lowerRowNeighbours[0]
-    lowerCenterCharacter = lowerRowNeighbours[0]
-    lowerRightCharacter = lowerRowNeighbours[0]
+    lowerCenterCharacter = lowerRowNeighbours[1]
+    lowerRightCharacter = lowerRowNeighbours[2]
 
     newState = selfCharacter
 
-    if selfCharacter == '0': # If Normal and there is an infected close, be Susceptible
-        if leftCharacter == '2' or rightCharacter == '2':
+    if selfCharacter == '0': # If Normal and there is an Infected close, be Susceptible
+        if leftCharacter == '2' or rightCharacter == '2' or upperLeftCharacter == '2' or upperRightCharacter == '2'\
+                or lowerLeftCharacter == '2' or lowerRightCharacter == '2' or upperCenterCharacter == '2' or lowerCenterCharacter == '2':
             newState = '1'
     else:
         if selfCharacter == '1': # if Susceptible, calculate the probability to be Infected
@@ -36,51 +38,28 @@ def getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours):
 
     return newState
 
-def getNewState(previousNeighboursState):
-    newState = '0'
-
-    leftCharacter = previousNeighboursState[0]
-    selfCharacter = previousNeighboursState[1]
-    rightCharacter = previousNeighboursState[2]
-
-    newState = selfCharacter
-
-    if selfCharacter == '0': # If Normal and there is an infected close, be Susceptible
-        if leftCharacter == '2' or rightCharacter == '2':
-            newState = '1'
-    else:
-        if selfCharacter == '1': # if Susceptible, calculate the probability to be Infected
-            if (2 - round(np.random.uniform(0.0, 1.0), 10)) <= beta:
-                newState = '2'
-            else:
-                newState = '0'
-        else:
-            if selfCharacter == '2': # if Infected, calculate the probability to be Susceptible 'to recover'
-                if (1 - round(np.random.uniform(0.0, 1.0), 10)) <= gamma:
-                    newState = '1'
-
-    return newState
-
+# Parameters
 beta = 1.4247 # Chance to get S from neighbouring I
 gamma = 0.14286 # Chance to get from I to R (or normal in our case)
+
+# Init values
 susceptibleCharacter = 'S'
 infectedCharacter ='I'
-normalCharacter = ' '
-maxgenerations = 30
+normalCharacter = '_'
+extremeEndValue = '0'
+maxGenerations = 30
 
 cellX = 10
 cellY = 10
 
-extremeEndValue = '0'
-
-t_start = 0.0
-t_end = maxgenerations
-t_inc = 1
-t_range = np.arange(t_start, t_end + t_start, t_inc)
+timeStart = 0.0
+timeEnd = maxGenerations
+timeStep = 1
+timeRange = np.arange(timeStart, timeEnd + timeStart, timeStep)
 
 universeList = []
 
-# Init, get random I
+# Randomise first state
 for j in range(cellY):
     universe = ''.join(random.choice('000000002') for i in range(cellX))
     universeList.append(universe)
@@ -91,12 +70,16 @@ InitVariables = [InitSusceptibles, InitInfected, 0.0, 0.0]
 
 RES = [InitVariables]
  
-for i in range(maxgenerations):
+for i in range(maxGenerations):
 
     # Print the current generation
     print "Generation %3i:  " % i
+    rowLabel = "  "
+    for l in range(cellX):
+        rowLabel += str(l) + " "
+    print rowLabel
     for k in range(cellY):
-        print universeList[k].replace('0', normalCharacter).replace('1', susceptibleCharacter).replace('2', infectedCharacter)
+        print "%s %s" % (k, universeList[k].replace('0', normalCharacter + " ").replace('1', susceptibleCharacter + " ").replace('2', infectedCharacter + " "))
 
     # Store the counts of I, S and the time iteration
     zeroCount = 0
@@ -108,27 +91,24 @@ for i in range(maxgenerations):
         twoCount += universeList[k].count('2')
     RES.append([zeroCount, oneCount, twoCount, i])
 
-    # Calculate the next generation
+    # Put extreme ends neighbouring cells temporarily
+    oldUniverseList = []
     for k in range(cellY):
-        universeList[k] = extremeEndValue + universeList[k] + extremeEndValue
+        oldUniverseList.append(extremeEndValue + universeList[k] + extremeEndValue)
 
-        #universeList[k] = ''.join(
-        #    getNewState(
-        #        universeList[k][i:i+3]
-        #    ) for i in range(cellX)
-
-        # newUniverseList = universeList
-        newUniverseList = ''
-        for i in range(cellX):
+    for k in range(cellY):
+        newUniverseRow = ''
+        for j in range(cellX):
             upperRowNeighbours = '000'
             lowerRowNeighbours = '000'
-            currentRowNeighbours = universeList[k][i:i+3]
+            currentRowNeighbours = oldUniverseList[k][j:j+3]
             if (k - 1) >= 0:
-                upperRowNeighbours = universeList[k-1][i:i+3]
+                upperRowNeighbours = oldUniverseList[k-1][j:j+3]
             if (k + 1) < cellY:
-                lowerRowNeighbours = universeList[k+1][i:i+3]
-            newUniverseList += getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours)
-        universeList[k] = newUniverseList
+                lowerRowNeighbours = oldUniverseList[k+1][j:j+3]
+
+            newUniverseRow += getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours)
+            universeList[k] = newUniverseRow
 
 print RES
 
