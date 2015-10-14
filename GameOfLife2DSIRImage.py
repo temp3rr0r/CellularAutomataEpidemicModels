4d52113b6a35ba6c0ed9be460c4a5217b6e5a5f3
TeMp3rr0r <madks@yahoo.gr>
2015-10-14 22:01:25 +0200

    Working animation SIR 2d

---
+++ GameOfLife2DSIRImage.py
@@ -0,0 +1,272 @@
+import random
+import numpy as np
+import pylab as pl
+from operator import itemgetter
+# Import a library of functions called 'pygame'
+import pygame
+from math import pi
+import time
+
+def drawGenerationUniverse(cellCountX, cellCountY, universeTimeSeries):
+    # Initialize the game engine
+    pygame.init()
+
+    # Define the colors we will use in RGB format
+    BLACK = (  0,   0,   0)
+    WHITE = (255, 255, 255)
+    BLUE =  (  0,   0, 255)
+    GREEN = (  0, 255,   0)
+    RED =   (255,   0,   0)
+    ORANGE =   (255,   165,   0)
+
+    # Set the height and width of the screen
+    screenHeight = 500
+    screenWidth = 500
+    size = [screenHeight, screenWidth]
+    screen = pygame.display.set_mode(size)
+    screen.fill(WHITE)
+    cellSize = screenHeight / cellCountX
+
+    #Loop until the user clicks the close button.
+    clock = pygame.time.Clock()
+
+    #while 1:
+    # Make sure game doesn't run at more than 60 frames per second
+    mainloop = True
+    FPS = 60                           # desired max. framerate in frames per second.
+    playtime = 0
+    cycletime = 0
+    interval = .15#.15 # how long one single images should be displayed in seconds
+    picnr = 0
+
+    #for currentStep in range(simulationIterations):
+    currentTimeStep = 0
+
+    while mainloop:
+        milliseconds = clock.tick(FPS)  # milliseconds passed since last frame
+        seconds = milliseconds / 1000.0 # seconds passed since last frame (float)
+        playtime += seconds
+        cycletime += seconds
+        if cycletime > interval:
+
+            if currentTimeStep >= simulationIterations:
+                currentTimeStep = 0
+            else:
+                currentTimeStep += 1
+            #pygame.time.delay(3000)
+            pygame.display.set_caption("TimeStep %3i:  " % currentTimeStep)
+
+            picnr += 1
+            if picnr > 5:
+                picnr = 0
+            cycletime = 0
+
+            currentColour = BLACK
+            for currentRow in range(cellCountY):# Draw a solid rectangle
+                for currentColumn in range (cellCountX):
+                    # rect(Surface, color, Rect, width=0) -> Rect
+                    #pygame.draw.rect(screen, BLACK, [150, 10, 50, 20])
+                    if currentTimeStep > 0 and currentTimeStep < simulationIterations:
+                        if universeTimeSeries[currentTimeStep][currentRow][currentColumn] == '0':
+                            currentColour = WHITE
+                        if universeTimeSeries[currentTimeStep][currentRow][currentColumn] == '1':
+                            currentColour = ORANGE
+                        if universeTimeSeries[currentTimeStep][currentRow][currentColumn] == '2':
+                            currentColour = RED
+                        if universeTimeSeries[currentTimeStep][currentRow][currentColumn] == '3':
+                            currentColour = BLUE
+
+                        pygame.draw.rect(screen, currentColour, [currentColumn * cellSize, currentRow * cellSize, (currentColumn + 1)
+                                                                 * cellSize, (currentRow + 1) * cellSize])
+
+        # This MUST happen after all the other drawing commands.
+        pygame.display.flip()
+        #pygame.time.delay(1)
+        # Go ahead and update the screen with what we've drawn.
+        #time.sleep(3)
+
+    # print "TimeStep %3i:  " % currentTimeStep
+    # rowLabel = "  "
+    # for l in range(cellCountX):
+    #     rowLabel += str(l) + " "
+    # print rowLabel
+    # for currentRow in range(cellCountY):
+    #     print "%s %s" % (currentRow, universeList[currentRow].replace('0', normalCharacter + " ").replace('1', susceptibleCharacter + " ").
+    #                      replace('2', infectedCharacter + " ").replace('3', recoveredCharacter + " "))
+
+''' Print the current generation '''
+def printGenerationUniverse(currentTimeStep, cellCountX, cellCountY, normalCharacter, susceptibleCharacter, infectedCharacter, recoveredCharacter):
+    print "TimeStep %3i:  " % currentTimeStep
+    rowLabel = "  "
+    for l in range(cellCountX):
+        rowLabel += str(l) + " "
+    print rowLabel
+    for currentRow in range(cellCountY):
+        print "%s %s" % (currentRow, universeList[currentRow].replace('0', normalCharacter + " ").replace('1', susceptibleCharacter + " ").
+                         replace('2', infectedCharacter + " ").replace('3', recoveredCharacter + " "))
+
+''' This method calculates the new state of the cell based on Van Neumann neighborhood '''
+def getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours):
+
+    leftCharacter = currentRowNeighbours[0]
+    selfCharacter = currentRowNeighbours[1]
+    rightCharacter = currentRowNeighbours[2]
+
+    upperLeftCharacter = upperRowNeighbours[0]
+    upperCenterCharacter = upperRowNeighbours[1]
+    upperRightCharacter = upperRowNeighbours[2]
+
+    lowerLeftCharacter = lowerRowNeighbours[0]
+    lowerCenterCharacter = lowerRowNeighbours[1]
+    lowerRightCharacter = lowerRowNeighbours[2]
+
+    newState = selfCharacter
+
+    if selfCharacter == '0': # If Normal and there is an Infected close, be Susceptible
+        if leftCharacter == '2' or rightCharacter == '2' or\
+            upperLeftCharacter == '2' or upperRightCharacter == '2' or upperCenterCharacter == '2'\
+                or lowerLeftCharacter == '2' or lowerRightCharacter == '2' or lowerCenterCharacter == '2':
+            newState = '1'
+    elif selfCharacter == '1': # if Susceptible, calculate the probability to be Infected
+        #betaChance = (2 - np.random.normal(0.5, 1.0)) # NORMAL
+        betaChance = (2 - np.random.uniform()) # UNIFORM
+        #betaChance = (2 - (np.random.poisson(2) % 10) * 0.1) # POISSON
+        if betaChance > 0 and betaChance < beta:
+            newState = '2'
+        else:
+            newState = '1'
+    elif selfCharacter == '2': # if Infected, calculate the probability to be Recovered 'to recover'
+        gammaChance = (1 - np.random.normal(0.5, 1.0)) # NORMAL
+        #gammaChance = (1 - np.random.uniform()) # UNIFORM
+        #gammaChance = (1 - (np.random.poisson(2) % 10) * 0.1) # POISSON
+
+        if gammaChance < gamma and gammaChance > 0:
+            newState = '3'
+    elif selfCharacter == '3': # Recovered, immune for a while
+        rhoChance = (1 - np.random.normal(0.5, 1.0)) # NORMAL
+        #rhoChance = (1 - np.random.uniform()) # UNIFORM
+        #rhoChance = (1 - (np.random.poisson(2) % 10) * 0.1) # POISSON
+
+        if rhoChance < rho and rhoChance > 0:
+            newState = '0'
+
+    return newState
+
+# SIS Model Parameters
+beta = 1.12247 # Chance to get S from neighbouring I
+gamma = 0.0 # Chance to get from I to R (or normal in our case)
+rho = 0.0 # Chance ot get from R to normal (Loss of immunity rate)
+simulationIterations = 100
+cellCountX = 33
+cellCountY = 33
+
+# Init values
+susceptibleCharacter = 'S'
+recoveredCharacter = 'R'
+infectedCharacter ='I'
+normalCharacter = '_'
+extremeEndValue = '0'
+timeStart = 0.0
+timeEnd = simulationIterations
+timeStep = 1
+timeRange = np.arange(timeStart, timeEnd + timeStart, timeStep)
+universeList = []
+
+# Randomise first state
+for currentColumn in range(cellCountY):
+    universe = ''.join(random.choice('000000002') for universeColumn in range(cellCountX))
+    universeList.append(universe)
+
+# TODO: Fix init state vars
+InitSusceptibles = 0.0
+InitInfected = 0.0
+InitRecovered = 0.0
+InitVariables = [InitSusceptibles, InitInfected, 0.0, 0.0, 0.0]
+
+RES = [InitVariables]
+
+universeTimeSeries = []
+
+# Main Execution loop
+for currentTimeStep in range(simulationIterations):
+
+    # Print the current generation
+    if currentTimeStep < 4:
+        printGenerationUniverse(currentTimeStep, cellCountX, cellCountY, normalCharacter, susceptibleCharacter, infectedCharacter, recoveredCharacter)
+
+    # Store the counts of I, S and the time iteration
+    zeroCount = 0
+    oneCount = 0
+    twoCount = 0
+    threeCount = 0
+    for currentRow in range(cellCountY):
+        zeroCount += universeList[currentRow].count('0')
+        oneCount += universeList[currentRow].count('1')
+        twoCount += universeList[currentRow].count('2')
+        threeCount += universeList[currentRow].count('3')
+    RES.append([zeroCount, oneCount, twoCount, threeCount, currentTimeStep])
+
+    # Put extreme ends neighbouring cells temporarily on the old universe
+    oldUniverseList = []
+    toCopyUniverseList = []
+    for currentRow in range(cellCountY):
+        oldUniverseList.append(extremeEndValue + universeList[currentRow] + extremeEndValue)
+        toCopyUniverseList.append(universeList[currentRow])
+
+    universeTimeSeries.append(toCopyUniverseList)
+
+    for currentRow in range(cellCountY):
+        newUniverseRow = ''
+        for currentColumn in range(cellCountX):
+            upperRowNeighbours = '000'
+            lowerRowNeighbours = '000'
+            currentRowNeighbours = oldUniverseList[currentRow][currentColumn:currentColumn+3]
+            if (currentRow - 1) >= 0:
+                upperRowNeighbours = oldUniverseList[currentRow-1][currentColumn:currentColumn+3]
+            if (currentRow + 1) < cellCountY:
+                lowerRowNeighbours = oldUniverseList[currentRow+1][currentColumn:currentColumn+3]
+
+            newUniverseRow += getNewState2D(currentRowNeighbours, upperRowNeighbours, lowerRowNeighbours)
+            universeList[currentRow] = newUniverseRow
+
+#print RES
+
+#print(universeTimeSeries)
+
+#Ploting
+pl.subplot(4, 1, 1)
+pl.plot(map(itemgetter(4), RES), map(itemgetter(2), RES), '-r', label='Infected')
+pl.plot(map(itemgetter(4), RES), map(itemgetter(0), RES), '-b', label='Normal')
+pl.legend(loc=0)
+pl.title('Infected and Normal')
+pl.xlabel('Time')
+pl.ylabel('Count')
+
+pl.subplot(4, 1, 2)
+pl.plot(map(itemgetter(4), RES), map(itemgetter(0), RES), '-b', label='Normal')
+pl.legend(loc=0)
+pl.title('Susceptibles and Normal')
+pl.xlabel('Time')
+pl.ylabel('Count')
+
+pl.subplot(4, 1, 3)
+pl.plot(map(itemgetter(4), RES), map(itemgetter(1), RES), '-r', label='Susceptibles')
+pl.plot(map(itemgetter(4), RES), map(itemgetter(2), RES), '-b', label='Infected')
+pl.legend(loc=0)
+pl.title('Susceptibles and Infected')
+pl.xlabel('Susceptibles')
+pl.ylabel('Infected')
+
+pl.subplot(4, 1, 4)
+pl.plot(map(itemgetter(4), RES), map(itemgetter(3), RES), '-r', label='Recovered')
+pl.plot(map(itemgetter(4), RES), map(itemgetter(0), RES), '-b', label='Normal')
+pl.plot(map(itemgetter(4), RES), map(itemgetter(1), RES), '-r', label='Susceptibles')
+pl.legend(loc=0)
+pl.title('Recovered and Normal')
+pl.xlabel('Time')
+pl.ylabel('Count')
+
+
+pl.show()
+
+drawGenerationUniverse(cellCountX, cellCountY, universeTimeSeries)
